34a1c20d252f5713e3003d3deb6011673f495867
Nicolas Torres <nicolas_daniel.torres@stud.tu-darmstadt.de>
2025-06-02 19:30:17 +0200

    feat: use catppuccin colorscheme in nvim

---
+++ .config/nvim/lua/plugins/general.lua
@@ -0,0 +1,54 @@
+-- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
+--
+-- In your plugin files, you can:
+-- * add extra plugins
+-- * disable/enabled LazyVim plugins
+-- * override the configuration of LazyVim plugins
+
+return {
+  -- Set catppuccin colorscheme
+  { "catppuccin/nvim", name = "catppuccin" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+
+  -- add more treesitter parsers
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "cpp",
+        "go",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "rust",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+    },
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shellcheck",
+        "shfmt",
+      },
+    },
+  },
+}
