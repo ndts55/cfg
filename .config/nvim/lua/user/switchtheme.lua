fdb07fd4ae95c5d9ac41d1aeb5dab06e28e188c7
Nicolas Torres <nicolastorres55@gmail.com>
2025-06-04 00:10:11 +0200

    feat: switch neovim theme when gnome theme switches without polling

---
+++ .config/nvim/lua/user/switchtheme.lua
@@ -0,0 +1,25 @@
+local M = {}
+
+function M.switch_theme(theme)
+  local background = (theme == "dark") and "dark" or "light"
+  vim.api.nvim_set_option_value("background", background, {})
+  if background == "light" then
+    vim.cmd("colorscheme catppuccin-latte")
+  else
+    vim.cmd("colorscheme catppuccin-mocha")
+  end
+end
+
+function M.setup()
+  vim.cmd([[ let $NVIM_LISTEN_ADDRESS = '/tmp/nvimsocket' ]])
+  vim.api.nvim_create_user_command("SwitchTheme", function(opts)
+    M.switch_theme(opts.args)
+  end, {
+    nargs = 1,
+    complete = function()
+      return { "light", "dark" }
+    end,
+  })
+end
+
+return M
