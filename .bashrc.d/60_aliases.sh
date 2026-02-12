97cb829272128142bb29c7807760407d03ad5410
Nicolas Torres <nicolastorres55@gmail.com>
2026-02-12 21:31:24 +0100

    feat: ls aliases

--- .bashrc.d/60_aliases.sh
+++ .bashrc.d/60_aliases.sh
@@ -1,5 +1,9 @@
 ## LS
-unalias ls
-alias ls="eza"
+if command -v eza >/dev/null 2>&1; then
+  unalias ls
+  alias ls="eza"
+  alias la="ls -la"
+  alias ll="ls -l"
+fi
 ## EDITOR
 alias vim="env -u NVIM_LISTEN_ADDRESS nvim"
